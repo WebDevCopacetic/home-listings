61eb2b2636daf008803474f69d82ad83290db964
Antoine <RandleAntoine@gmail.com>
2018-05-17 11:55:12 -0700

    added variables for the input fields

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("This is working properly!!")
+var $address = $('#address')
+var $bedrooms = $('#bedrooms')
+var $bathrooms = $('#bathrooms')
+var $list = $('#list')
+var $form = $('form')
+
+$form.on('submit', function (evt) {
+    evt.preventDefault () // This will keep us on the page as opposed to it refreshing when the submit button is clicked. 
+    var addressValue = $address.val()
+    var bedroomsValue = $bedrooms.val()
+    var bathroomsValue = $bathrooms.val()
+})
